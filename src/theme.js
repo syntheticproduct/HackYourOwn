c4ac592beda1ecf7ac14f548156dd27c48f30ed9
randomlylelo <lzhang.0693@gmail.com>
2020-06-01 20:37:56 -0400

    Initialized eslint.

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 let theme = createMuiTheme({
