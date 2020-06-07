0fca51d2a80b554217b325996d4af6e0ef66bbe3
toduko <teddyepich@gmail.com>
2020-06-07 11:52:36 +0300

    completely styled timeline

--- src/components/List/ProgramList.js
+++ src/components/List/ProgramList.js
@@ -1,6 +1,8 @@
 import { StyledList, TitleStyledList } from "components/List/StyledList.styled";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { List } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export const ProgramList = () => {
   return (
@@ -17,49 +19,156 @@ export const ProgramList = () => {
   );
 }
 
+const useTimelineStyles = makeStyles({
+  root: {
+    maxWidth: "70%",
+    margin: "0 auto"
+  },
+  dFlex: {
+    display: "flex"
+  },
+  left: {
+    width: "100%",
+    textAlign: "right"
+  },
+  right: {
+    width: "100%",
+    textAlign: "left"
+  },
+  middle: {
+    flexDirection: "column",
+    marginTop: "10px",
+  },
+  line: {
+    border: "1px solid #222",
+    width: "5%",
+    margin: "0 auto",
+    height: "100%"
+  },
+  lines: {
+    height: "100%",
+    margin: "0 auto",
+  }
+})
+
 const TimelineCircle = () => (
   <div 
-  style={{ 
-    width: "30px", height: "30px", borderRadius: "50%", 
-    background: "white", border: "2px solid orange", 
-    position: "relative", left: "-16px", top: "43px"
-  }} 
+    style={{ 
+      width: "30px", height: "30px", borderRadius: "50%", 
+      background: "white", border: "3px solid orange"
+    }} 
   />
 );
 
 export const TimelineList = () => {
+  const classes = useTimelineStyles();
   return (
-    <List style={{ borderLeft: "2px solid #000", margin: "0 auto", width: "40%" }}>
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 1:"
-        text="Git / GitHub, Basics of HTML, Basics of CSS"
-      />
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 2:"
-        text="Intro to JavaScript, DOM Manipulation"
-      />
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 3:"
-        text="Advanced JavaScript (ES6, classes, modules),Functional programming in JavaScript"
-      />
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 4:"
-        text="Javascript Data Structures, NPM, Webpack"
-      />
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 5-8:"
-        text="React, Node.js, Express.js, MongoDB, REST APIs*"
-      />
-      <TimelineCircle />
-      <TitleStyledList
-        title="Week 9-12:"
-        text="Capstone Project (full-stack MERN project)"
-      />
+    <List className={classes.root} >
+      <div className={classes.dFlex}>
+        <div className={classes.left}>
+          <TitleStyledList
+            title="Week 1:"
+            text="Git / GitHub, Basics of HTML, Basics of CSS"
+          />
+        </div>
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+          <TimelineCircle />
+          <div className={classes.dFlex, classes.lines}>
+            <div className={classes.line} />
+          </div>
+        </div>
+        <div className={classes.right} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+      </div>
+      <div className={classes.dFlex}>
+        <div className={classes.left} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+          <TimelineCircle />
+          <div className={classes.dFlex, classes.lines}>
+            <div className={classes.line} />
+          </div>
+        </div>
+        <div className={classes.right} >
+          <TitleStyledList
+            title="Week 2:"
+            text="Intro to JavaScript, DOM Manipulation"
+          />
+        </div>
+      </div>
+      <div className={classes.dFlex}>
+        <div className={classes.left} >
+          <TitleStyledList
+            title="Week 3:"
+            text="Advanced JavaScript (ES6, classes, modules), Functional programming in JavaScript"
+          />
+        </div>
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+          <TimelineCircle />
+          <div className={classes.dFlex, classes.lines}>
+            <div className={classes.line} />
+          </div>
+        </div>
+        <div className={classes.right} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+      </div>
+      <div className={classes.dFlex}>
+        <div className={classes.left} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+          <TimelineCircle />
+          <div className={classes.dFlex, classes.lines}>
+            <div className={classes.line} />
+          </div>
+        </div>
+        <div className={classes.right} >
+          <TitleStyledList
+            title="Week 4:"
+            text="Javascript Data Structures, NPM, Webpack"
+          />
+        </div>
+      </div>
+      <div className={classes.dFlex}>
+        <div className={classes.left}>
+          <TitleStyledList
+            title="Week 5-8:"
+            text="React, Node.js, Express.js, MongoDB, REST APIs*"
+          />
+        </div>
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+          <TimelineCircle />
+          <div className={classes.dFlex, classes.lines}>
+            <div className={classes.line} />
+          </div>
+        </div>
+        <div className={classes.right} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+      </div>
+      <div className={classes.dFlex}>
+        <div className={classes.left} style={{ width: useMediaQuery(theme => theme.breakpoints.down('sm')) ? 0 : "100%" }} />
+        <div 
+          className={classes.middle} 
+          style={{ order: useMediaQuery(theme => theme.breakpoints.down('sm')) ? -1 : 0 }}
+        >
+            <TimelineCircle />
+        </div>
+        <div className={classes.right} >
+          <TitleStyledList
+            title="Week 9-12:"
+            text="Capstone Project (full-stack MERN project)"
+          />
+        </div>
+      </div>
     </List>
   );
 };
